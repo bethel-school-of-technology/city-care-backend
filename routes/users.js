428eff509a2d6e5ccca5a4711558e14d2672a6e5
Jonathan Kemper <jonkemper@gmail.com>
2020-08-28 20:12:29 -0400

    user model update to remove dupe /register

--- routes/users.js
+++ routes/users.js
@@ -42,48 +42,6 @@ router.post('/register', function (req, res, next) {
     });
 });
 
-router.post('/register', function (req, res, next) {
-  models.users
-    .findOrCreate({
-      where: {
-        email: req.body.email
-      },
-      defaults: {
-        isOrg: req.body.isOrg,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        org_name: req.body.org_name,
-        contact_name: req.body.contact_name,
-        username: req.body.username,
-        phone: req.body.phone,
-        mobile_phone: req.body.mobile_phone,
-        fax: req.body.fax,
-        contact_method: req.body.contact_method,
-        address1: req.body.address1,
-        address2: req.body.address2,
-        city: req.body.city,
-        state: req.body.state,
-        county: req.body.county,
-        zip: req.body.zip,
-        password: authService.hashPassword(req.body.password),
-        isOrg: true,
-        deleted: false,
-        admin: true,
-        active: false
-      }
-    })
-    .spread(function (result, created) {
-      if (created) {
-        console.log(result);
-        res.status(200).json(result);
-      } else {
-        res.status(400).json({
-          message: 'Not going to happen sunshine!'
-        });
-      }
-    });
-});
-
 //Log a user in
 router.post('/login', function (req, res, next) {
   let fetchedUser;
