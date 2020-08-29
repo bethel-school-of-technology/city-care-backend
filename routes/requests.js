33ec02a4972e33235ddce97a3a48c91a88154eca
Jonathan Kemper <jonkemper@gmail.com>
2020-08-29 12:20:34 -0400

    updated create request method

--- routes/requests.js
+++ routes/requests.js
@@ -5,138 +5,122 @@ var mysql = require('mysql2');
 var models = require('../models');
 var authService = require('../services/auth');
 
-router.get('/create', function(req, res, next) {
-          res.status(200).json({message: 'You fetched the create request route.'})
-})
+router.get('/create', function (req, res, next) {
+  res.status(200).json({ message: 'You fetched the create request route.' });
+});
 //Create a user request
-router.post('/create', function(req, res, next) {
-          let token = req.headers['jwt'];
-          if(token) {
-                    authService.verifyUser(token).then(user => {
-                              if(user) {
-                                        models.requests.findOrCreate({
-                                                  where: { description: req.body.description},
-                                                  defaults: {
-                                                            category: req.body.category,
-                                                            sub_category: req.body.sub_category,
-                                                            deleted: false,
-                                                            user_id: user.id
-                                                  }
-                                        }).spread(function(result, created) {
-                                                  if(created) {
-                                                            console.log(result);
-                                                            res.status(200).json(result);
-                                                  } else {
-                                                            res.status(400).json({message: 'Sorry sunshine, not going to create that one!'})
-                                                  }
-                                        });
-                              } else {
-                                        res.status(500).json({message: 'Internal server error!'})
-                              }
-                    });
-          }
+router.post('/create', function (req, res, next) {
+  let token = req.headers['jwt'];
+  if (token) {
+    authService.verifyUser(token).then((user) => {
+      if (user) {
+        models.requests
+          .findOrCreate({
+            where: { name: req.body.name },
+            defaults: {
+              details: req.body.description,
+              needByDate: req.body.needByDate,
+              user_id: user.id
+            }
+          })
+          .spread(function (result, created) {
+            if (created) {
+              console.log(result);
+              res.status(200).json(result);
+            } else {
+              res.status(400).json({
+                message: 'Error creating request, please try again'
+              });
+            }
+          });
+      } else {
+        res.status(500).json({ message: 'Internal server error!' });
+      }
+    });
+  }
 });
-// router.post('/create', function(req, res, next) {
-//           let token = req.headers['jwt'];
-//           if(token) {
-//                     authService.verifyUser(token).then(user => {
-//                              if(user) {
-//                                        models.requests.findOrCreate({
-//                                                  where: { description: req.body.description },
-//                                                  defaults: {
-//                                                            category: req.body.category,
-//                                                            sub_category: req.body.sub_category,
-//                                                            deleted: false,
-//                                                            user_id: user.id
-//                                                  }
-//                                        }).spread(function(result, created) {
-//                                                  if(created) {
-//                                                            console.log(result);
-//                                                            res.status(200).json(result);
-//                                                  } else {
-//                                                            res.status(400).json({message: 'Can not create request!' })
-//                                                  }
-//                                        });
-//                              }
-//                     });
-//           }
-// });
 
-//Get all of the requests made by an individual 
-router.get('/requests', function(req, res, next) {
-          let token = req.headers['jwt'];
-          if(token) {
-                    authService.verifyUser(token).then(user => {
-                              if(user) {
-                                        models.requests.findAll({
-                                                  where: { user_id: user.id, deleted: false }
-                                        }).then(requests =>{
-                                                  console.log(requests);
-                                                  res.status(200).json(requests);
-                                        })
-                              } else { 
-                                        res.status(400).json({message: 'Can not find requests.'})
-                              }
-                    })
-          }
+//Get all of the requests made by an individual
+router.get('/requests', function (req, res, next) {
+  let token = req.headers['jwt'];
+  if (token) {
+    authService.verifyUser(token).then((user) => {
+      if (user) {
+        models.requests
+          .findAll({
+            where: { user_id: user.id, deleted: false }
+          })
+          .then((requests) => {
+            console.log(requests);
+            res.status(200).json(requests);
+          });
+      } else {
+        res.status(400).json({ message: 'Can not find requests.' });
+      }
+    });
+  }
 });
-//Get a single request made by the individual 
-router.get('/:id', function(req, res, next) {
-          let token = req.headers['jwt'];
-          let id = req.params.id;
-          if(token) {
-                    authService.verifyUser(token).then(user => {
-                              if(user) {
-                                        models.requests.findByPk(req.params.id)
-                                        .then(request => {
-                                                  console.log(request);
-                                                  res.status(200).json(request);
-                                        })
-                              } else {
-                                        res.status(401).json({message: 'Could not find request!'})
-                              }
-                    })
-          } else {
-                    res.status(500).json({ message: 'Internal server error!'})
-          }
+//Get a single request made by the individual
+router.get('/:id', function (req, res, next) {
+  let token = req.headers['jwt'];
+  let id = req.params.id;
+  if (token) {
+    authService.verifyUser(token).then((user) => {
+      if (user) {
+        models.requests.findByPk(req.params.id).then((request) => {
+          console.log(request);
+          res.status(200).json(request);
+        });
+      } else {
+        res.status(401).json({ message: 'Could not find request!' });
+      }
+    });
+  } else {
+    res.status(500).json({ message: 'Internal server error!' });
+  }
 });
 //Update a request
-router.put('/:id', function(req, res, next) {
-          let token = req.headers['jwt'];
-          let id = parseInt(req.params.id);
-          if(token) {
-                    authService.verifyUser(token).then(user => {
-                              if(user) {
-                                        models.requests.update({
-                                                  
-                                        })
-                              }
-                    })
-          }
+router.put('/:id', function (req, res, next) {
+  let token = req.headers['jwt'];
+  let id = parseInt(req.params.id);
+  if (token) {
+    authService.verifyUser(token).then((user) => {
+      if (user) {
+        models.requests.update({});
+      }
+    });
+  }
 });
 //Delete an existing request
-router.delete('/:id', function(req, res, next) {
-          let token = req.headers['jwt'];
-          let requestId = parseInt(req.params.id);
-          if(token) {
-             authService.verifyUser(token).then(user => {
-                if(user) {
-                   models.requests.update({
-                      deleted: true
-                   }, {
-                      where: { id: requestId }
-                   }).then(function(result) {
-                      if(result) {
-                         console.log(result);
-                         res.status(200).json({message: 'Listing marked for deletion!'})
-                      } else {
-                         res.status(400).json({message: 'You are not authorized to delete this listing!'})
-                      }
-                   })
-                } else {
-                   res.status(500).json({message: 'Internal server error!'})
-                }
-             })
-          }
-       });
+router.delete('/:id', function (req, res, next) {
+  let token = req.headers['jwt'];
+  let requestId = parseInt(req.params.id);
+  if (token) {
+    authService.verifyUser(token).then((user) => {
+      if (user) {
+        models.requests
+          .update(
+            {
+              deleted: true
+            },
+            {
+              where: { id: requestId }
+            }
+          )
+          .then(function (result) {
+            if (result) {
+              console.log(result);
+              res.status(200).json({ message: 'Listing marked for deletion!' });
+            } else {
+              res.status(400).json({
+                message: 'You are not authorized to delete this listing!'
+              });
+            }
+          });
+      } else {
+        res.status(500).json({ message: 'Internal server error!' });
+      }
+    });
+  }
+});
 module.exports = router;
