5f3494c6d1bfd8276bce5406983a861c80e20073
tbone7243 <tbone7243@gmail.com>
2020-08-31 21:00:49 -0400

    update

--- routes/requests.js
+++ routes/requests.js
@@ -31,7 +31,7 @@ router.post('/create', function (req, res, next) {
               res.status(200).json(created);
             } else {
               res.status(400).json({
-                message: 'Not today Satan!'
+                message: 'Not today Satan'
               });
             }
           });
@@ -63,20 +63,22 @@ router.get('/requests', function (req, res, next) {
   }
 });
 //Get all of the requests in the same zip code as the logged in user
-router.get('/county/requests', function (req, res, next) {
+router.get('/all/requests', function (req, res, next) {
   let token = req.headers['jwt'];
   if (token) {
     authService.verifyUser(token)
       .then(user => {
         if (user) { 
-          models.users
+          models.requests
             .findAll({
-              where: { id: user.id }, 
-              include: {model: models.requests}, 
-              where: {zip: user.zip }
+              where: { deleted: false }, 
+              include: {model: models.users}, 
+              where: {user_id: user.id }
             })
-            .then(zip_listings => {
-             res.status(200).json(zip_listings);
+            .then(requests => {
+              console.log(requests);
+             res.status(200).json(requests);
+
             })
         } else {
           res.status(400).json({ message: 'Whoops! Something went wrong!' })
