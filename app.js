87302afa8a64b504312bcb6acca55af9ddfe8ef7
tbone7243 <tbone7243@gmail.com>
2020-09-18 16:03:45 -0400

    Woring on deployment

--- app.js
+++ app.js
@@ -3,36 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const models = require('./models');
-//Auth0 Authentication
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
+
 
 const app = express();
 
 
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// //Import the models folder to use models within the application
-// var models = require('./models');
-const port = process.env.PORT || 3001;
 
-const jwtCheck = jwt({
-       secret: jwks.expressJwtSecret({
-         cache: true,
-         rateLimit: true,
-         jwksRequestsPerMinute: 5,
-         jwksUri: 'https://tekwebtek.us.auth0.com/.well-known/jwks.json'
-       }),
-       audience: 'https://city_care_api',
-       issuer: 'https://tekwebtek.us.auth0.com/',
-       algorithms: ['RS256']
-});
-app.use(jwtCheck);
-app.get('/authorized', function(req, res) {
-  res.send('Secured Resource!')
-})
 
 const cors = require('cors'); //Include Cross Origin Resource Sharing To Connect With Angular Front End
 
@@ -74,8 +50,8 @@ res.render('error');
 // app.get('/', (req,res) => {
 //           res.sendFile(process.cwd()+"/my-app/dist/angular-nodejs-example/index.html")
 //         });
-        
-        app.listen(port, () => {
+   const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;     
+  const server =  app.listen(port, function() {
             console.log(`Server listening on the port::${port}`);
         });
 //Connect to the MySQL Database
